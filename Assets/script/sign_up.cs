2ec9c04f1156417213c671e48805e53fc43699f1
mi1468 <mi1468@yahoo.com>
2018-07-23 15:31:30 +0430

    avale kar :)
    start

---
+++ Assets/script/sign_up.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sign_up : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void sign_up_btn()
+    {
+
+    }
+}
