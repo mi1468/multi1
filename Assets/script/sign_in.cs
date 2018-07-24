2cff090360bd5f41e45d50db0ee9f80d8ef8009a
mi1468 <mi1468@yahoo.com>
2018-07-24 18:21:10 +0430

    sing up avaliye

---
+++ Assets/script/sign_in.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using UnityEngine.UI;
+
+public class sign_in : MonoBehaviour {
+  
+    public string matn, email, name, pasword;
+    public InputField  in_email, in_pasword;
+    private SocketIOComponent socket;
+    public string Md5Sum(string strToEncrypt)
+    {
+        System.Text.UTF8Encoding ue = new System.Text.UTF8Encoding();
+        byte[] bytes = ue.GetBytes(strToEncrypt);
+
+        // encrypt bytes
+        System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+        byte[] hashBytes = md5.ComputeHash(bytes);
+
+        // Convert the encrypted bytes back to a string (base 16)
+        string hashString = "";
+
+        for (int i = 0; i < hashBytes.Length; i++)
+        {
+            hashString += System.Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+        }
+
+        return hashString.PadLeft(32, '0');
+    }
+	// Use this for initialization
+    void Start()
+    {
+
+        GameObject go = GameObject.Find("SocketIO");
+        socket = go.GetComponent<SocketIOComponent>();
+       
+    }
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void sign_btn()
+    {
+      
+        email = in_email.text;
+        pasword = in_pasword.text;
+        pasword = Md5Sum(pasword + "hehe");
+        matn = "{\"email\":\"" + email + "\",\"pasword\":\"" + pasword + "\"}";
+        socket.Emit("sign_in", new JSONObject(matn));
+    }
+}
--- Assets/script/sign_up.cs
+++ Assets/script/sign_up.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SocketIO;
+using UnityEngine.UI;
 
 public class sign_up : MonoBehaviour {
-
+    public GameObject email_tekrari_panel;
+    public InputField in_name,in_email,in_pasword;
+    public string matn,email,name,pasword;
+    private SocketIOComponent socket;
+    
 	// Use this for initialization
+
 	void Start () {
-		
+        email_tekrari_panel.SetActive(false);
+        GameObject go = GameObject.Find("SocketIO");
+        socket = go.GetComponent<SocketIOComponent>();
+        socket.On("email_tekrari", email_tekrari);
 	}
 	
 	// Update is called once per frame
@@ -15,6 +25,39 @@ public class sign_up : MonoBehaviour {
 	}
     public void sign_up_btn()
     {
+        name = in_name.text;
+        email = in_email.text;
+        pasword = in_pasword.text;
+        pasword = Md5Sum(pasword+"hehe");
+         matn = "{\"name\":\"" + name + "\",\"email\":\"" + email + "\",\"pasword\":\"" + pasword + "\"}";
+         socket.Emit("add user", new JSONObject(matn));
+    }
+    private void email_tekrari(SocketIOEvent e)
+    {
 
+        email_tekrari_panel.SetActive(true);
+    }
+    public void email_tekrari_btn()
+    {
+        email_tekrari_panel.SetActive(false);
+    }
+    public string Md5Sum(string strToEncrypt)
+    {
+        System.Text.UTF8Encoding ue = new System.Text.UTF8Encoding();
+        byte[] bytes = ue.GetBytes(strToEncrypt);
+
+        // encrypt bytes
+        System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+        byte[] hashBytes = md5.ComputeHash(bytes);
+
+        // Convert the encrypted bytes back to a string (base 16)
+        string hashString = "";
+
+        for (int i = 0; i < hashBytes.Length; i++)
+        {
+            hashString += System.Convert.ToString(hashBytes[i], 16).PadLeft(2, '0');
+        }
+
+        return hashString.PadLeft(32, '0');
     }
 }
